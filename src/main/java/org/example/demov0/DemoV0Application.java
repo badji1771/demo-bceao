b09ce0460f1a70de21a952f6ef4a1a54774ffe5c
mbadji <mbadji1771@gmail.com>
2024-05-23 15:46:59 +0000

    initial commit

---
+++ src/main/java/org/example/demov0/DemoV0Application.java
@@ -0,0 +1,20 @@
+package org.example.demov0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoV0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoV0Application.class, args);
+	}
+
+	@RequestMapping("/hello")
+	public String hello(){
+		return "Hello Bceao !!!";
+	}
+}
---
+++ src/test/java/org/example/demov0/DemoV0ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demov0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoV0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
